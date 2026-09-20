ce3e883fc141117ef35879ff79f803565f81eb6a
selekttimdoo-pixel <selekttimdoo@gmail.com>
2026-09-20 12:19:26 +0200

    Bump Android app to v0.4.1 roman voice

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vipla.bato"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.0-continuity"
+        versionCode = 8
+        versionName = "0.4.1-roman-voice"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures { compose = true }
